d7b9faca66ce73caa493c9dda16aeb08a8389dbe
Matheus Souza araujo <matheus@MacBook-Pro-de-Matheus.local>
2025-02-15 15:47:04 -0300

    resolved exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const App = () => {
-  return <div>App React</div>;
+
+  const produtos = [
+    {
+      id: 1,
+      nome: 'Smartphone',
+      preco: 'R$ 2000',
+      cores: ['#29d8d5', '#252a34', '#fc3766'],
+    },
+    {
+      id: 2,
+      nome: 'Notebook',
+      preco: 'R$ 3000',
+      cores: ['#ffd045', '#d4394b', '#f37c59'],
+    },
+    {
+      id: 3,
+      nome: 'Tablet',
+      preco: 'R$ 1500',
+      cores: ['#365069', '#47c1c8', '#f95786'],
+    },
+  ];
+  
+
+  return(
+    <ul>
+      {produtos.filter(({ preco }) => Number(preco.slice(2,preco.length) > 1500)).map(({ id, nome, preco, cores }) => (
+        <li key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
